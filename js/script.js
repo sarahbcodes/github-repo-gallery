ca91a841c8521f1bd70bdc4b3fa31a772e23d587
sarah_b <sarah.am.barton@gmail.com>
2023-04-19 16:52:29 -0700

    Fetched repo data from GitHub account

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const overview = document.querySelector(".overview");
 const username = "sarahbcodes";
+const repoList = document.querySelector(".repo-list");
 
 const users = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
-    console.log(data);
+    // console.log(data);
     displayUserInfo(data);
 };
 
@@ -12,7 +13,7 @@ users();
 
 const displayUserInfo = function (data) {
     const div = document.createElement("div");
-    div.classList.add(".user-info");
+    div.classList.add("user-info");
     div.innerHTML = `    
     <figure>
     <img alt="user avatar" src=${data.avatar_url} />
@@ -24,6 +25,23 @@ const displayUserInfo = function (data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>`;
   overview.append(div);
+  publicRepos();
 };
 
+//fetch public repo info//
+const publicRepos = async function () {
+  const gitRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await gitRepos.json();
+  // console.log(repoData);
+  displayRepoInfo(repoData);
+};
 
+//display repo info//
+const displayRepoInfo = async function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
+};
